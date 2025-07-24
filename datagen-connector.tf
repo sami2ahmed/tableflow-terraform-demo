54b12b1760cadfd3b74211419b8a5f9576597762
“sami2ahmed” <samiahmedkeys@gmail.com>
2025-07-24 17:41:16 +0800

    Add customer telephone datagen connector and update Kafka topics
    - Added customer-telephone-datagen Kafka topic
    - Added customer telephone datagen connector with custom Avro schema
    - Updated .gitignore file
    - Schema includes customer_number, customer_name, and telephone fields with predefined options

--- datagen-connector.tf
+++ datagen-connector.tf
@@ -61,3 +61,76 @@ resource "confluent_connector" "users_datagen" {
   }
 }
 
+resource "confluent_connector" "customer_telephone_datagen" {
+  environment {
+    id = confluent_environment.my_environment.id
+  }
+  kafka_cluster {
+    id = confluent_kafka_cluster.kafka-cluster.id
+  }
+
+  config_sensitive = {}
+
+  config_nonsensitive = {
+    "connector.class"          = "DatagenSource"
+    "name"                     = "customer-telephone-datagen-connector"
+    "kafka.auth.mode"          = "SERVICE_ACCOUNT"
+    "kafka.service.account.id" = confluent_service_account.my_service_account.id
+    "kafka.topic"              = confluent_kafka_topic.customer_telephone_datagen.topic_name
+    "output.data.format"       = "AVRO"
+    "schema.keyfield"          = "customer_number"
+    "schema.string"            = jsonencode({
+      "type": "record",
+      "name": "CustomerTelephone",
+      "fields": [
+        {
+          "name": "customer_number",
+          "type": {
+            "type": "string",
+            "arg.properties": {
+              "options": [
+                "123456789", "123456790", "123456791", "123456792", "123456793", "123456794",
+                "123456795", "123456796", "123456797", "123456798", "123456799", "123456800"
+              ]
+            }
+          }
+        },
+        {
+          "name": "customer_name",
+          "type": {
+            "type": "string",
+            "arg.properties": {
+              "options": [
+                "Gayle Fisher", "Napoleon Keith", "Carmella Schaefer", "Ronald Duncan", "Francis Stout",
+                "Scottie Knox", "Rosa Delgado", "Ana Larson", "Willian Best", "John Smith", "Jane Doe", "Michael Johnson"
+              ]
+            }
+          }
+        },
+        {
+          "name": "telephone",
+          "type": {
+            "type": "string",
+            "arg.properties": {
+              "options": [
+                "63488686", "67200789", "62928181", "68874074", "64674415", "62551633",
+                "64414489", "63624606", "68966982", "65123456", "67654321", "69876543"
+              ]
+            }
+          }
+        }
+      ]
+    })
+    "tasks.max"                = "1"
+    "max.interval"             = "1000"
+  }
+
+  depends_on = [
+    confluent_kafka_acl.write-basic-cluster
+  ]
+
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
--- kafka-topics.tf
+++ kafka-topics.tf
@@ -21,3 +21,15 @@ resource "confluent_kafka_topic" "users" {
     secret = confluent_api_key.kafka-api-key.secret
   }
 }
+
+resource "confluent_kafka_topic" "customer_telephone_datagen" {
+  kafka_cluster {
+    id = confluent_kafka_cluster.kafka-cluster.id
+  }
+  topic_name         = "customer-telephone-datagen"
+  rest_endpoint      = confluent_kafka_cluster.kafka-cluster.rest_endpoint
+  credentials {
+    key    = confluent_api_key.kafka-api-key.id
+    secret = confluent_api_key.kafka-api-key.secret
+  }
+}
